95e059c18b417b98fba3892ff364d956824b0073
Cloyd Balongkit <cloydbalongkit1@gmail.com>
2023-10-24 07:53:12 +0800

    Adding functionality on schedule of loads but not included the wire design

--- engineering_app/__init__.py
+++ engineering_app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
-app.config['SECRET_KEY'] = os.environ.get('ENG_AP_SECRET_KEY')
+# app.config['SECRET_KEY'] = os.environ.get('ENG_AP_SECRET_KEY')
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 from engineering_app import routes
--- engineering_app/form.py
+++ engineering_app/form.py
@@ -40,20 +40,18 @@ class PowerCalc(FlaskForm):
 class SchedLoads(FlaskForm):
     item1 = IntegerField('Item/s', validators=[InputRequired(), NumberRange(min=1, max=100)])
     description1 = StringField('Description', validators=[DataRequired(), Length(min=1, max=20)])
-    unit1 = StringField('Unit', validators=[DataRequired(), Length(min=1, max=20)])
     qty1 = IntegerField('Qty', validators=[InputRequired(), NumberRange(min=0)])
     power1 = IntegerField('Power', validators=[InputRequired(), NumberRange(min=0)])
 
     item2 = IntegerField('Item/s', validators=[InputRequired(), NumberRange(min=1, max=100)])
     description2 = StringField('Description', validators=[DataRequired(), Length(min=1, max=20)])
-    unit2 = StringField('Unit', validators=[DataRequired(), Length(min=1, max=20)])
     qty2 = IntegerField('Qty', validators=[InputRequired(), NumberRange(min=0)])
     power2 = IntegerField('Power', validators=[InputRequired(), NumberRange(min=0)])
 
     item3 = IntegerField('Item/s', validators=[InputRequired(), NumberRange(min=1, max=100)])
     description3 = StringField('Description', validators=[DataRequired(), Length(min=1, max=20)])
-    unit3 = StringField('Unit', validators=[DataRequired(), Length(min=1, max=20)])
     qty3 = IntegerField('Qty', validators=[InputRequired(), NumberRange(min=0)])
     power3 = IntegerField('Power', validators=[InputRequired(), NumberRange(min=0)])
 
+    voltage = IntegerField('System Voltage', validators=[InputRequired(), NumberRange(min=1)])
     submit = SubmitField('Calculate')
